6cbccb94ae34663db8ef95dde1c037639c454d18
Jeffrey P <jeffp.web@gmail.com>
2025-02-12 14:57:24 -0800

    Additional Storybook setup

---
+++ .storybook/preview.ts
@@ -0,0 +1,13 @@
+import '../src/index.css'; // Import your Tailwind CSS
+
+export const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+}; 
